0fd336537d1b8dc81b5f37bbc9fcb3d297373c76
Jørgen Skogmo <js@kanda.dk>
2015-08-19 09:41:43 +0200

    0.0.2 auto

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 // mirror server example
 
-var port 	= 8080;
-var pack 	= require('./package.json');
+var port = 8080;
+
 var express = require('express');
 var chalk 	= require('chalk');
-var mirror 	= require('./mirror/main.js');
-
-var mex 			= require('./mongo-express');
-var mex_config 		= require('./mongo-express/config.default.js');
-var mex_middleware 	= require('./mongo-express/middleware');
+var pack 	= require('./package.json');
+var mirror 	= require('./mirror/main.js')
+var mex 	= require('./mongo-express');
+var mex_cnf = require('./mongo-express/config.default.js');
+var mex_mw 	= require('./mongo-express/middleware');
 
 var app = express();
 
-/*
-app.use(function(req, res, next){
-	console.log( req.url );
-    next();
-});
-*/
-
 app.get('/version', function(req, res){
-	var msg = {'status':'ok', 'code':'VERSION', 'message': pack.name +' v.'+ pack.version};
-	console.log( chalk.grey( msg ));
-	res.json(msg);
+	res.json({'status':'ok', 'code':'VERSION', 'message': pack.name +' v.'+ pack.version});
 });
 
 //TODO: add auth middleware
@@ -37,7 +28,7 @@ mirror.add('countries', ['name', 'countryCode']);
 
 
 // Connect Mongo-Express
-app.use('/mex', mex_middleware(mex_config));
+app.use('/mex', mex_mw(mex_cnf));
 console.log( chalk.green("Enabling Mongo-Express on /mex") );
 
 // Start serving
