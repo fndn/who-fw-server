4c1b577f38414c2c2bc96c067914e420dd96a77f
Jørgen Skogmo <js@fndn.dk>
2015-09-16 14:45:43 +0200

    0.0.7 auto

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ app.all('/*', function(req, res, next){
 		//console.log('req.headers', req.headers );
 
 		var token = req.headers['x-auth-token'];
+		console.log('token', token );
+		
 		if( token == undefined ){
 			res.send({"status":"error", "msg":"access denied"});
 		}else if( valid_tokens.indexOf(token) == -1 ){
