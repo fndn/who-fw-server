212dbe1e78ec8fae1a6546b792c9dd9572e6a2a0
Jørgen Skogmo <js@fndn.dk>
2015-10-18 22:30:55 +0200

    jaa

--- mirror/main.js
+++ mirror/main.js
@@ -204,13 +204,12 @@ module.exports.add = function(_name, fields){
 						var key = keys[j];
 						//console.log('testing key:', key );
 						if( re.exec(key) !== null ){
-							
+							console.log('# matched key:', key, 'val:', item[key] );
 
 							items[i][key] = items[i][key].replace('products', 'products/img');
 							items[i][key] = items[i][key].replace('jpg', 'jpeg');
 							items[i][key] = '<a href="//'+ items[i][key] +'">'+ items[i][key] +'</a>';
-
-							console.log('# matched key:', key, 'val:', item[key], items[i][key] );
+							console.log('> result:', items[i][key] );
 						}
 					}
 				}
